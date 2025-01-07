2cdad57737e52b693f0e6b11fd88b2baa4604bcc
yash <yash@ith.tech>
2025-01-07 12:11:57 +0530

    code merge

--- src/environment/environment.tsx
+++ src/environment/environment.tsx
@@ -1,5 +1,5 @@
-const baseUrl = "http://localhost:5000/api";
-// const baseUrl = "https://chirag-backend.onrender.com/api";
+// const baseUrl = "http://localhost:5000/api";
+const baseUrl = "https://chirag-backend.onrender.com/api";
 // const baseUrl = "https://chirag.solminica.com/api";
 const REACT_APP_CASHFREE_APP_ID='TEST10323053441509d8bfad032f1b6835032301';
 
