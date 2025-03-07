1e6804f8b3faf9218e0575dbea67fc278a5811d5
Yash jangid <yash@ith.tech>
2025-03-07 11:50:39 +0530

    phoen pe integration

--- src/environment/environment.tsx
+++ src/environment/environment.tsx
@@ -3,4 +3,4 @@ const baseUrl = "https://chirag-backend.onrender.com/api";
 // const baseUrl = "https://chirag.solminica.com/api";
 const PHONEPE_API_KEY = 'ba46ef40-99df-40b2-a4d9-7885ca35aecb';
 
-// export { baseUrl, PHONEPE_API_KEY };
+export { baseUrl, PHONEPE_API_KEY };
